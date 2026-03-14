8ededfc57f861b86c8359983a514fd4ac476b9b8
Lanari Tech Ltd <yakinnsanzumuhire@gmail.com>
2026-03-14 19:40:24 +0200

    dghf

--- android/app/src/main/java/com/communityroster/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.communityroster.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.blessingcommunity.app',
-  appName: 'Blessing Community Roster',
-  webDir: 'dist'
-};
-
-export default config;
